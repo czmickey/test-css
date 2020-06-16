80203420845b54d98f1dc8566930f3a840d82743
Michal Oktabec <michal.oktabec@lmc.eu>
2020-06-16 16:45:26 +0200

    Prepare App

--- src/App.js
+++ src/App.js
@@ -1,26 +1,38 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import './App.css'
 
-function App() {
+function App () {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Nadpis</h1>
+      <p>
+        Týž div v eh bod no mečem dřeva spolehlivé – že mžilo tě horny! Via ať
+        padaly mládencům a opatrně. Kup se tichém dne zlýho sledujte, vleklo no
+        hm v týž kasař k šuměly ho labutí to zajíkal z stránce budoucnost. Moh
+        rozšafný čikulí rup blázen neoženil. Eh buben žádným oč rubu ošklivosti
+        lampu včely jet obcí! Cit tu prs útočit útočit v strká má čin vraha něm
+        mordy. Mu mima nedal slabá vodil tři uf lustr. Sna hm hlav, nevypadl,
+        nápadnější, vyhodí – levitaci, že ah civile, co květnu záhy slušní
+        hubený. Si ze ironií, běhají něco. Lež že vrh ó tě nikde té nedá ať
+        odkašlal situace ó vlasů aparát. Váš nedovedl ať vlčákem labe spěšně
+        stavy obce.
+      </p>
+      <p>
+        He větu vůz svíjela, štýrsku, vylovila ať služce vyškrtnut i mrkaje tahá
+        ví dávaje. Hekal vše od ni dáma posluha plodů. Bác fuk ho roh dveřích,
+        jich. Oblafnout zjistíme abys vzrušeně s dovedla odpykanou přimlouvalajá
+        špičce. Boženy šófl uf osmdesát – uf haló hanák jak paničky feldvébl ba
+        vy neznamená i hned oč důmyslu hýká pobádal mapě kamenné lístek
+        půlhodina kříž ti nadával. Kulku síň dřímal ty osmi pět pátrali rádi ni
+        jé kroutit. Taky vyryl ke anglicky. Dva němá má mrzká má škoda himl
+        přines sevřené Dastychu sedni o složeném vedou. Nervu boj sláva, děj
+        bičem, jo hra mě poslu měření. Ke my hodili jektal ho basta šíp bídě
+        pěstí růst už tyhlety my ostrým holane nepřednášel. Ses nevnímal nůž
+        konal, rozumně pří myši té poněvadž, o býval: abeced bál, píše, fotelu
+        pusť ta ze břicha sousede?
+      </p>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
